bc9f478af554b24e76c643c8194a2d8aad5d64d1
Shivu1sys <154315247+Shivu1sys@users.noreply.github.com>
2026-07-31 00:43:44 +0530

    Update SELF_JOIN.sql

--- SQL-Portfolio/02_Joins/SELF_JOIN.sql
+++ SQL-Portfolio/02_Joins/SELF_JOIN.sql
@@ -1,1 +1,38 @@
+-- =====================================================
+-- Topic: SELF JOIN
+-- Description: Join a table with itself
+-- =====================================================
 
+-- Example 1: Display employees and their managers
+
+SELECT
+    e.EmployeeID,
+    e.EmployeeName AS Employee,
+    m.EmployeeName AS Manager
+FROM Employees e
+LEFT JOIN Employees m
+ON e.ManagerID = m.EmployeeID;
+
+---------------------------------------------------------
+
+-- Example 2: Display agents reporting to Team Leads
+
+SELECT
+    a.AgentName,
+    l.AgentName AS TeamLead
+FROM Agents a
+LEFT JOIN Agents l
+ON a.TeamLeadID = l.AgentID;
+
+---------------------------------------------------------
+
+-- Business Scenario
+
+-- Retrieve employees along with their reporting managers.
+
+SELECT
+    e.EmployeeName,
+    m.EmployeeName AS ReportingManager
+FROM Employees e
+LEFT JOIN Employees m
+ON e.ManagerID = m.EmployeeID;
