520d1e3503397deb60f2cec0892105212be65c65
Shivu1sys <154315247+Shivu1sys@users.noreply.github.com>
2026-07-30 21:07:34 +0530

    Update SELECT.sql

--- SQL-Portfolio/01_Basic_SQL/SELECT.sql
+++ SQL-Portfolio/01_Basic_SQL/SELECT.sql
@@ -1,1 +1,53 @@
+-- =====================================================
+-- Topic: SELECT Statement
+-- Description: Retrieve data from a database table
+-- =====================================================
 
+-- Example 1: Retrieve all columns
+
+SELECT *
+FROM Customers;
+
+---------------------------------------------------------
+
+-- Example 2: Retrieve specific columns
+
+SELECT CustomerID,
+       CustomerName,
+       City
+FROM Customers;
+
+---------------------------------------------------------
+
+-- Example 3: Retrieve unique cities
+
+SELECT DISTINCT City
+FROM Customers;
+
+---------------------------------------------------------
+
+-- Example 4: Rename column using alias
+
+SELECT CustomerName AS Customer,
+       City AS Location
+FROM Customers;
+
+---------------------------------------------------------
+
+-- Example 5: Retrieve customer names in alphabetical order
+
+SELECT CustomerName
+FROM Customers
+ORDER BY CustomerName;
+
+---------------------------------------------------------
+
+-- Business Scenario
+
+-- Retrieve customer details for business reporting.
+
+SELECT CustomerID,
+       CustomerName,
+       Email,
+       City
+FROM Customers;
