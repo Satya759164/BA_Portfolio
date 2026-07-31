532c18d861c0fb0af99ae7377e9b8604cce61168
Shivu1sys <154315247+Shivu1sys@users.noreply.github.com>
2026-07-31 16:12:55 +0530

    Update MIN.sql

--- SQL-Portfolio/03_Aggregate_Functions/MIN.sql
+++ SQL-Portfolio/03_Aggregate_Functions/MIN.sql
@@ -1,1 +1,25 @@
+-- =====================================================
+-- Topic: MIN()
+-- Description: Returns the minimum value
+-- =====================================================
 
+-- Example 1: Lowest order amount
+
+SELECT MIN(Amount) AS LowestOrderValue
+FROM Orders;
+
+---------------------------------------------------------
+
+-- Example 2: Earliest ticket creation date
+
+SELECT MIN(CreatedDate) AS FirstTicket
+FROM Tickets;
+
+---------------------------------------------------------
+
+-- Business Scenario
+
+-- Find the earliest support ticket created.
+
+SELECT MIN(CreatedDate) AS FirstTicketDate
+FROM Tickets;
