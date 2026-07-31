51d5709b6fb4760165f839dda5691221c413f258
Shivu1sys <154315247+Shivu1sys@users.noreply.github.com>
2026-07-31 16:11:59 +0530

    Update COUNT.sql

--- SQL-Portfolio/03_Aggregate_Functions/COUNT.sql
+++ SQL-Portfolio/03_Aggregate_Functions/COUNT.sql
@@ -1,1 +1,34 @@
+-- =====================================================
+-- Topic: COUNT()
+-- Description: Counts the number of records
+-- =====================================================
 
+-- Example 1: Total number of customers
+
+SELECT COUNT(*) AS TotalCustomers
+FROM Customers;
+
+---------------------------------------------------------
+
+-- Example 2: Total number of tickets
+
+SELECT COUNT(*) AS TotalTickets
+FROM Tickets;
+
+---------------------------------------------------------
+
+-- Example 3: Total resolved tickets
+
+SELECT COUNT(*) AS ResolvedTickets
+FROM Tickets
+WHERE Status = 'Resolved';
+
+---------------------------------------------------------
+
+-- Business Scenario
+
+-- Find the total number of open support tickets.
+
+SELECT COUNT(*) AS OpenTickets
+FROM Tickets
+WHERE Status = 'Open';
