308ed836ea150e03bfcc9266a235ed3a0e1c5a0b
Shivu1sys <154315247+Shivu1sys@users.noreply.github.com>
2026-07-31 16:25:54 +0530

    Update CTMS_Ticket_Analysis.sql

--- SQL-Portfolio/05_Business_Case_Studies/CTMS_Ticket_Analysis.sql
+++ SQL-Portfolio/05_Business_Case_Studies/CTMS_Ticket_Analysis.sql
@@ -1,1 +1,49 @@
+-- =====================================================
+-- Topic: CTMS Ticket Analysis
+-- Description: Analyze support ticket data
+-- =====================================================
 
+-- Example 1: Open tickets
+
+SELECT *
+FROM Tickets
+WHERE Status = 'Open';
+
+---------------------------------------------------------
+
+-- Example 2: High priority tickets
+
+SELECT *
+FROM Tickets
+WHERE Priority = 'High';
+
+---------------------------------------------------------
+
+-- Example 3: Tickets by status
+
+SELECT Status,
+       COUNT(*) AS TotalTickets
+FROM Tickets
+GROUP BY Status;
+
+---------------------------------------------------------
+
+-- Example 4: Tickets assigned to each agent
+
+SELECT AgentID,
+       COUNT(*) AS TotalTickets
+FROM Tickets
+GROUP BY AgentID;
+
+---------------------------------------------------------
+
+-- Business Scenario
+
+-- Display the top 5 agents handling the highest number of tickets.
+
+SELECT AgentID,
+       COUNT(*) AS TotalTickets
+FROM Tickets
+GROUP BY AgentID
+ORDER BY TotalTickets DESC
+LIMIT 5;
