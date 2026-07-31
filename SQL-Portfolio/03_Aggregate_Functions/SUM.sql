41795821b3821782ee20c85c196e5d5a689e5d0d
Shivu1sys <154315247+Shivu1sys@users.noreply.github.com>
2026-07-31 16:12:18 +0530

    Update SUM.sql

--- SQL-Portfolio/03_Aggregate_Functions/SUM.sql
+++ SQL-Portfolio/03_Aggregate_Functions/SUM.sql
@@ -1,1 +1,25 @@
+-- =====================================================
+-- Topic: SUM()
+-- Description: Calculates the total value
+-- =====================================================
 
+-- Example 1: Total sales amount
+
+SELECT SUM(Amount) AS TotalSales
+FROM Orders;
+
+---------------------------------------------------------
+
+-- Example 2: Total ticket resolution hours
+
+SELECT SUM(ResolutionHours) AS TotalHours
+FROM Tickets;
+
+---------------------------------------------------------
+
+-- Business Scenario
+
+-- Calculate total revenue generated.
+
+SELECT SUM(Amount) AS TotalRevenue
+FROM Orders;
