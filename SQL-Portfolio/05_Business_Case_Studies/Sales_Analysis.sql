9877020a002e43e8a4a346da01bdf26e120c8ac9
Shivu1sys <154315247+Shivu1sys@users.noreply.github.com>
2026-07-31 16:26:31 +0530

    Update Sales_Analysis.sql

--- SQL-Portfolio/05_Business_Case_Studies/Sales_Analysis.sql
+++ SQL-Portfolio/05_Business_Case_Studies/Sales_Analysis.sql
@@ -1,1 +1,36 @@
+-- =====================================================
+-- Topic: Sales Analysis
+-- Description: Analyze sales performance
+-- =====================================================
 
+-- Example 1: Total sales
+
+SELECT SUM(Amount) AS TotalSales
+FROM Orders;
+
+---------------------------------------------------------
+
+-- Example 2: Average order value
+
+SELECT AVG(Amount) AS AverageOrderValue
+FROM Orders;
+
+---------------------------------------------------------
+
+-- Example 3: Highest order amount
+
+SELECT MAX(Amount) AS HighestOrderValue
+FROM Orders;
+
+---------------------------------------------------------
+
+-- Business Scenario
+
+-- Display the top 5 highest-value orders.
+
+SELECT OrderID,
+       CustomerID,
+       Amount
+FROM Orders
+ORDER BY Amount DESC
+LIMIT 5;
