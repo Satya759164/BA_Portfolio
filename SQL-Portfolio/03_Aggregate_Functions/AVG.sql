3b2f3388f209666acd9ff7ba5c464768d16f5e61
Shivu1sys <154315247+Shivu1sys@users.noreply.github.com>
2026-07-31 16:12:35 +0530

    Update AVG.sql

--- SQL-Portfolio/03_Aggregate_Functions/AVG.sql
+++ SQL-Portfolio/03_Aggregate_Functions/AVG.sql
@@ -1,1 +1,25 @@
+-- =====================================================
+-- Topic: AVG()
+-- Description: Calculates the average value
+-- =====================================================
 
+-- Example 1: Average ticket resolution time
+
+SELECT AVG(ResolutionHours) AS AverageResolutionTime
+FROM Tickets;
+
+---------------------------------------------------------
+
+-- Example 2: Average order value
+
+SELECT AVG(Amount) AS AverageOrderValue
+FROM Orders;
+
+---------------------------------------------------------
+
+-- Business Scenario
+
+-- Find the average resolution time for support tickets.
+
+SELECT AVG(ResolutionHours) AS AverageResolutionTime
+FROM Tickets;
