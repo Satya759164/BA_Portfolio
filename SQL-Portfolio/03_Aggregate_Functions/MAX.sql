3cff0c32e32b8d2d597d50ae6dcb5de42f09ec2f
Shivu1sys <154315247+Shivu1sys@users.noreply.github.com>
2026-07-31 16:13:13 +0530

    Update MAX.sql

--- SQL-Portfolio/03_Aggregate_Functions/MAX.sql
+++ SQL-Portfolio/03_Aggregate_Functions/MAX.sql
@@ -1,1 +1,25 @@
+-- =====================================================
+-- Topic: MAX()
+-- Description: Returns the maximum value
+-- =====================================================
 
+-- Example 1: Highest order amount
+
+SELECT MAX(Amount) AS HighestOrderValue
+FROM Orders;
+
+---------------------------------------------------------
+
+-- Example 2: Latest ticket creation date
+
+SELECT MAX(CreatedDate) AS LatestTicket
+FROM Tickets;
+
+---------------------------------------------------------
+
+-- Business Scenario
+
+-- Find the most recently created support ticket.
+
+SELECT MAX(CreatedDate) AS LatestTicketDate
+FROM Tickets;
